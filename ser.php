1bac70bc9c607d2f6afb1a6c8fa31ff2e881ccc1
ivan006 <ivan.copeland2015@gmail.com>
2020-12-22 15:08:53 +0200

    1

---
+++ ser.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$array = '[
+[12,27,49],
+[37,67,27,14,13,54,29,44],
+[38,33,20,65],
+[],
+[41,70,59,16,61,53,27]
+]';
+foreach ($array as $key => $value){
+  foreach $value as $key_2 => $value_2){
+    $array[$key][$key_2] = $value_2;
+  }
+}
+$array = json_decode($array);
+foreach ($array as $key => $value) {
+  echo serialize($value)."<br>";
+}
+
+
+?>
+
+</body>
+</html>
